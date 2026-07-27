e98e0abcc94896e2608ba4c673783dca36f5f1e0
Raffinnaisse <rabbit23113@gmail.com>
2026-07-27 11:22:57 +0300

    feat: server in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"wallet/internal/app/api"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewServer(handler http.Handler) *Server {
+	httpServer := &http.Server{
+		Addr:           ":8080",
+		MaxHeaderBytes: 1 << 20,
+		Handler:        handler,
+	}
+	return &Server{httpServer: httpServer}
+}
+
+func (server *Server) Start() {
+	err := server.httpServer.ListenAndServe()
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+}
+
+func (server *Server) Shutdown() {
+	err := server.httpServer.Shutdown(context.Background())
+	if err != nil {
+		slog.Error(err.Error())
+	}
+}
+
+func main() {
+	handler := api.NewHandler()
+	srv := NewServer(handler)
+	srv.Start()
+}
